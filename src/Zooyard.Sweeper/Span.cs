c1a3acfc99071c2fe72800161bdfbac27c7fd20f
WuTao <WuTao@DESKTOP-OFFBS2H>
2018-08-16 23:35:15 +0800

    add dapper trace project

---
+++ src/Zooyard.Sweeper/Span.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zooyard.Sweeper
+{
+    public class Span
+    {
+        /// <summary>
+        /// ID
+        /// </summary>
+        public int Id { get; set; }
+        /// <summary>
+        /// Parent ID
+        /// </summary>
+        public int ParentId { get; set; }
+        /// <summary>
+        /// Trace Id
+        /// </summary>
+        public long TraceId { get; set; }
+        /// <summary>
+        /// span Name
+        /// </summary>
+        public string Name { get; set; }
+    }
+}
