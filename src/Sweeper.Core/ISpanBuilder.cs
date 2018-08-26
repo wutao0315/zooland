fc6b86a0a09c8bb781336eacc220f3ba9b5d2403
WuTao <WuTao@DESKTOP-OFFBS2H>
2018-08-26 22:26:43 +0800

    sweeper  unit test

---
+++ src/Sweeper.Core/ISpanBuilder.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sweeper.Core
+{
+    public interface ISpanBuilder
+    {
+
+        /**
+         * A shorthand for addReference(References.CHILD_OF, parent).
+         *
+         * <p>
+         * If parent==null, this is a noop.
+         */
+        ISpanBuilder asChildOf(ISpanContext parent);
+
+        /**
+         * A shorthand for addReference(References.CHILD_OF, parent.context()).
+         *
+         * <p>
+         * If parent==null, this is a noop.
+         */
+        ISpanBuilder asChildOf(ISpan parent);
+
+        /**
+         * Add a reference from the Span being built to a distinct (usually parent) Span. May be called multiple times
+         * to represent multiple such References.
+         *
+         * <p>
+         * If
+         * <ul>
+         * <li>the {@link Tracer}'s {@link ScopeManager#active()} is not null, and
+         * <li>no <b>explicit</b> references are added via {@link SpanBuilder#addReference}, and
+         * <li>{@link SpanBuilder#ignoreActiveSpan()} is not invoked,
+         * </ul>
+         * ... then an inferred {@link References#CHILD_OF} reference is created to the
+         * {@link ScopeManager#active()} {@link SpanContext} when either {@link SpanBuilder#startActive(boolean)} or
+         * {@link SpanBuilder#start} is invoked.
+         *
+         * @param referenceType the reference type, typically one of the constants defined in References
+         * @param referencedContext the SpanContext being referenced; e.g., for a References.CHILD_OF referenceType, the
+         *                          referencedContext is the parent. If referencedContext==null, the call to
+         *                          {@link #addReference} is a noop.
+         *
+         * @see io.opentracing.References
+         */
+        ISpanBuilder addReference(string referenceType, ISpanContext referencedContext);
+
+        /**
+         * Do not create an implicit {@link References#CHILD_OF} reference to the {@link ScopeManager#active()}).
+         */
+        ISpanBuilder ignoreActiveSpan();
+
+        /** Same as {@link Span#setTag(String, String)}, but for the span being built. */
+        ISpanBuilder withTag(string key, string value);
+
+        /** Same as {@link Span#setTag(String, boolean)}, but for the span being built. */
+        ISpanBuilder withTag(string key, bool value);
+
+        /** Same as {@link Span#setTag(String, Number)}, but for the span being built. */
+        ISpanBuilder withTag(string key, int value);
+
+        /** Specify a timestamp of when the Span was started, represented in microseconds since epoch. */
+        ISpanBuilder withStartTimestamp(long microseconds);
+
+        /**
+         * Returns a newly started and activated {@link Scope}.
+         *
+         * <p>
+         * The returned {@link Scope} supports try-with-resources. For example:
+         * <pre><code>
+         *     try (Scope scope = tracer.buildSpan("...").startActive(true)) {
+         *         // (Do work)
+         *         scope.span().setTag( ... );  // etc, etc
+         *     }
+         *     // Span does finishes automatically only when 'finishSpanOnClose' is true
+         * </code></pre>
+         *
+         * <p>
+         * If
+         * <ul>
+         * <li>the {@link Tracer}'s {@link ScopeManager#active()} is not null, and
+         * <li>no <b>explicit</b> references are added via {@link SpanBuilder#addReference}, and
+         * <li>{@link SpanBuilder#ignoreActiveSpan()} is not invoked,
+         * </ul>
+         * ... then an inferred {@link References#CHILD_OF} reference is created to the
+         * {@link ScopeManager#active()}'s {@link SpanContext} when either
+         * {@link SpanBuilder#start()} or {@link SpanBuilder#startActive} is invoked.
+         *
+         * <p>
+         * Note: {@link SpanBuilder#startActive(boolean)} is a shorthand for
+         * {@code tracer.scopeManager().activate(spanBuilder.start(), finishSpanOnClose)}.
+         *
+         * @param finishSpanOnClose whether span should automatically be finished when {@link Scope#close()} is called
+         * @return a {@link Scope}, already registered via the {@link ScopeManager}
+         *
+         * @see ScopeManager
+         * @see Scope
+         */
+        IScope startActive(bool finishSpanOnClose);
+
+
+        /**
+         * Like {@link #startActive()}, but the returned {@link Span} has not been registered via the
+         * {@link ScopeManager}.
+         *
+         * @see SpanBuilder#startActive(boolean)
+         * @return the newly-started Span instance, which has *not* been automatically registered
+         *         via the {@link ScopeManager}
+         */
+        ISpan start();
+    }
+}
---
+++ test/SweeperTest/activate_deactivate.cs
@@ -0,0 +1,150 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sweeper.Core;
+using Sweeper.Core.Support;
+using Sweeper.Mock;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace SweeperTest
+{
+    [TestClass]
+    public class activate_deactivate
+    {
+
+        private readonly MockTracer tracer = new MockTracer(new AutoFinishScopeManager(),
+            new MockTracer.TextMapPropagator());
+        //private final ScheduledExecutorService service = Executors.newScheduledThreadPool(10);
+
+        public class RunnableAction
+        {
+            private readonly AutoFinishScope.Continuation continuation;
+           public RunnableAction(AutoFinishScope scope)
+            {
+                continuation = scope.capture();
+                Console.WriteLine("Action created");
+            }
+            public void run()
+            {
+                Console.WriteLine("Action started");
+                AutoFinishScope scope = continuation.activate();
+
+                try
+                {
+                    Thread.Sleep(1000);
+                    //TimeUnit.SECONDS.sleep(1); // without sleep first action can finish before second is started
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.StackTrace);
+                }
+
+                // set random tag starting with 'test_tag_' to test that finished span has all of them
+                scope.span().setTag("test_tag_" + new Random().Next(), "random");
+
+                scope.Dispose();
+                Console.WriteLine("Action finished");
+            }
+
+        }
+
+        [TestMethod]
+        public void test_one_scheduled_action()
+        {
+            var entrythread = entryThread();
+            entrythread.Start();
+
+            while (TestUtils.finishedSpansSize(tracer) < 1)
+            {
+
+            }
+
+            List<MockSpan> finished = tracer.FinishedSpans;
+            Assert.AreEqual(1, finished.Count);
+
+            Assert.AreEqual(1, getTestTagsCount(finished[0]));
+        }
+
+        [TestMethod]
+        public void test_two_scheduled_actions()
+        {
+            var entryThread = entryThreadWithTwoActions();
+            entryThread.Start();
+            entryThread.Join(10_000);
+            // Entry thread is completed but Actions are still running (or even not started)
+
+            while (TestUtils.finishedSpansSize(tracer) < 1)
+            {
+
+            }
+
+            List<MockSpan> finished = tracer.FinishedSpans;
+            Assert.AreEqual(1, finished.Count);
+
+            // Check that two actions finished and each added to span own tag ('test_tag_{random}')
+            Assert.AreEqual(2, getTestTagsCount(finished[0]));
+        }
+
+        private int getTestTagsCount(MockSpan mockSpan)
+        {
+            int tagCounter = 0;
+            foreach (var tagKey in mockSpan.Tags.Keys)
+            {
+                if (tagKey.StartsWith("test_tag_"))
+                {
+                    tagCounter++;
+                }
+            }
+            return tagCounter;
+        }
+
+        /**
+         * Thread will be completed before action completed.
+         */
+        private Thread entryThread()
+        {
+            return new Thread(new ThreadStart(() => {
+                Console.WriteLine("Entry thread started");
+                using (IScope scope = tracer.buildSpan("parent").startActive(false))
+                {
+                    var action = new RunnableAction((AutoFinishScope)scope);
+
+                    // Action is executed at some time and we are not able to check status
+                    //service.schedule(action, 500, TimeUnit.MILLISECONDS);
+
+                    new Timer(new TimerCallback((state)=> { action.run(); }),null,500,0);
+                    Console.WriteLine("Entry thread finished");
+                };
+            }));
+        }
+
+        /**
+         * Thread will be completed before action completed.
+         */
+        private Thread entryThreadWithTwoActions()
+        {
+
+            return new Thread(new ThreadStart(()=> {
+                Console.Write("Entry thread 2x started");
+                using (IScope scope = tracer.buildSpan("parent").startActive(false))
+                {
+                    var action = new RunnableAction((AutoFinishScope)scope);
+                    var action2 = new RunnableAction((AutoFinishScope)scope);
+
+                    Random random = new Random();
+
+                    new Timer(new TimerCallback((state) => { action.run(); }), null, random.Next(1000) + 100, 0);
+                    new Timer(new TimerCallback((state) => { action2.run(); }), null, random.Next(1000) + 100, 0);
+                    // Actions are executed at some time and most likely are running in parallel
+                    //service.schedule(action, random.Next(1000) + 100, TimeUnit.MILLISECONDS);
+                    //service.schedule(action2, random.Next(1000) + 100, TimeUnit.MILLISECONDS);
+                    Console.WriteLine("Entry thread 2x finished");
+                }
+
+            }));
+        }
+    }
+}
