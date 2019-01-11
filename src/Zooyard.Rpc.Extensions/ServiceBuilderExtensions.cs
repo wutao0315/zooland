2a263c7ec18e5c967b4d8f8c7cfe3d141f0ef855
jweis <wutao0315@qq.com>
2019-01-11 17:54:01 +0800

    add rpc host service

---
+++ src/Zooyard.Rpc.Extensions/ServiceBuilderExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using Zooyard.Core;
+
+namespace Zooyard.Rpc.Extensions
+{
+    public static class ServiceBuilderExtensions
+    {
+        public static void AddRpc(this IServiceCollection services)
+        {
+            services.AddHostedService<ZoolandHostedService>();
+        }
+    }
+}
---
+++ src/Zooyard.Rpc.Extensions/ZoolandHostedService.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Zooyard.Core;
+
+namespace Zooyard.Rpc.Extensions
+{
+    public class ZoolandHostedService : IHostedService
+    {
+        private readonly ILogger _logger;
+        private readonly IServer _server;
+
+        public ZoolandHostedService(ILogger<ZoolandHostedService> logger, IServer server)
+        {
+            _logger = logger;
+            _server = server;
+        }
+
+        public async Task StartAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Zooland started...");
+            Console.WriteLine("Zooland started...");
+            _server.Export();
+            await Task.CompletedTask;
+        }
+
+        public async Task StopAsync(CancellationToken cancellationToken)
+        {
+            _logger.LogInformation("Zooland stopped...");
+            Console.WriteLine("Zooland stopped...");
+            _server.Dispose();
+            await Task.CompletedTask;
+        }
+    }
+}
