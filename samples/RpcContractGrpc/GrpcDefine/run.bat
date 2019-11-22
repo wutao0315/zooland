d3f751d5dab892bff38c3fb81331286db904e943
jweis <wutao0315@qq.com>
2019-11-22 09:11:46 +0800

    upgrade lib version

--- samples/RpcContractGrpc/GrpcDefine/run.bat
+++
@@ -1,8 +0,0 @@
-
-echo 生成csharp文件到项目GrpcImpls项目中，编译该项目即可；
-
-protoc.exe -I ./ --csharp_out ../gen-csharp  helloService.proto --grpc_out ../gen-csharp --plugin=protoc-gen-grpc=grpc_csharp_plugin.exe
-
-echo 生成完成
-pause
-
